502322c39e0157998960e42c31da7e92e75fce11
dakeng <123maple_leaves@sina.com>
2018-02-24 20:47:25 +0800

    '我的页面修改'

--- GBeanMallProject/src/screen/mine/Mine.js
+++ GBeanMallProject/src/screen/mine/Mine.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Text, Image, Button} from 'react-native';
-import {styles} from './../../common/modules/styles';
+import {Text, Image, Button, View, StyleSheet} from 'react-native';
+import {styles, deviceWidth} from './../../common/modules/styles';
 
-export default class CartScreen extends Component{
+export default class MineScreen extends Component{
     static navigationOptions = {
         tabBarLabel: '我的',
         tabBarIcon: ({tintColor}) => (
@@ -16,7 +16,22 @@ export default class CartScreen extends Component{
 
     render(){
         return(
-            <Text>Mine</Text>
-        )
+            <View>
+                <View style={MineStyles.accountContainer}>
+                    <Image source={require('./../../img/head.png')}/>
+                    <Text>登录</Text>
+                </View>
+            </View>
+        );
     }
 }
+
+const MineStyles = StyleSheet.create({
+    accountContainer: {
+        padding: 16,
+        backgroundColor: '#fff',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
