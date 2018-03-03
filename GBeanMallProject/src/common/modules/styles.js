c23d5ec1ddac672b3cb68fd47dc0db96eaf09206
dakeng <0_0liyuan@sina.com>
2018-03-04 02:07:14 +0800

    登录页面ui

--- GBeanMallProject/src/common/modules/styles.js
+++ GBeanMallProject/src/common/modules/styles.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
 const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#ebebeb',
@@ -9,6 +10,15 @@ const styles = StyleSheet.create({
         width: 20,
         height: 20,
     },
+    verticalAlign: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    spaceBetween: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
 });
 
-export {styles, deviceWidth};
+export {styles, deviceWidth, deviceHeight};
--- GBeanMallProject/src/screen/sign-in-or-up/SignInOrUp.js
+++ GBeanMallProject/src/screen/sign-in-or-up/SignInOrUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {Text, Image, View} from 'react-native';
-import {styles, deviceWidth} from './../../common/modules/styles';
+import {Text, Image, View, TextInput, TouchableOpacity} from 'react-native';
+import {styles, deviceWidth, deviceHeight} from './../../common/modules/styles';
 
 class Header extends Component {
     render(){
@@ -14,6 +14,7 @@ export default class SignInOrUp extends Component {
     static navigationOptions = {
         headerStyle: {
             backgroundColor: '#e94f37',
+            elevation: 0,
         },
         headerTitleStyle: {
             fontSize: 20,
@@ -21,10 +22,45 @@ export default class SignInOrUp extends Component {
         headerTintColor: '#fff',
     }
 
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+
     render(){
         return (
-            <View style={pageStyles.container}>
-                <Text>登录</Text>
+            <View style={[pageStyles.container]}>
+                <View style={[pageStyles.inputContainer, styles.verticalAlign]}>
+                    <TextInput
+                        style={[pageStyles.input, pageStyles.unInput]}
+                        placeholder="用户名"
+                        placeholderTextColor="#bcbcbc"
+                        underlineColorAndroid={'transparent'}
+                        onChangeText={text => this.setState({username: text})}
+                    />
+                    <TextInput
+                        secureTextEntry={true}
+                        style={[pageStyles.input, pageStyles.pwInput]}
+                        placeholder="密码"
+                        placeholderTextColor="#bcbcbc"
+                        underlineColorAndroid={'transparent'}
+                        onChangeText={text => this.setState({password: text})}
+                    />
+                    <TouchableOpacity style={[pageStyles.btn, styles.verticalAlign]}>
+                        <Text style={pageStyles.btnText}>登录</Text>
+                    </TouchableOpacity>
+                    <View style={[styles.spaceBetween, pageStyles.subContainer]}>
+                        <TouchableOpacity>
+                            <Text style={[pageStyles.subText, pageStyles.leftText]}>忘记密码？</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={[pageStyles.subText]}>注册账户</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -35,6 +71,51 @@ let pageStyles = {
 
     },
     container: {
+        width: deviceWidth,
+        height: deviceHeight,
         backgroundColor: '#e94f37',
+    },
+    inputContainer:{
+        marginTop: 12,
+    },
+    input: {
+        width: deviceWidth/1.12,
+        height: 54,
+        backgroundColor: '#fff',
+        padding: 16,
+        fontSize: 16
+    },
+    unInput: {
+        borderTopLeftRadius: 2,
+        borderTopRightRadius: 2,
+    },
+    pwInput: {
+        marginTop: -0.7,
+        borderBottomLeftRadius: 2,
+        borderBottomRightRadius: 2,
+        borderTopWidth: 0.3,
+        borderTopColor: '#dcdcdc',
+    },
+    btn: {
+        width: deviceWidth/1.12,
+        height: 48,
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        marginTop: 12,
+        borderRadius: 2,
+    },
+    btnText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    subContainer: {
+        width: deviceWidth/1.12,
+        marginTop: 12,
+    },
+    subText: {
+        color: '#fff',
+        fontSize: 14,
+    },
+    leftText: {
+        color: 'rgba(255, 255, 255, 0.5)',
     }
 }
